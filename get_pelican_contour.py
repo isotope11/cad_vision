cdecb739ca604c52093280ad3d15cf740917f227
laird foret <laird@isotope11.com>
2016-01-06 21:45:52 -0600

    put new scaling code in get_pelican_contour.py

--- get_pelican_contour.py
+++ get_pelican_contour.py
@@ -44,7 +44,7 @@ def get_contour(Img_PathandFilename = 'temp_image_file', opaque="--opaque", fill
                 edges = cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
                 #edges = auto_canny(edges)
                 #edges  = cv2.GaussianBlur(edges, (3,3), 0)
-                edges = cv2.Canny(edges,30,190)
+                edges = cv2.Canny(edges,30,180)
                 #ret,edges = cv2.threshold(edges, 127,255,cv2.THRESH_BINARY)
                 #edges  = cv2.GaussianBlur(edges, (5,5), 0)
                  
@@ -77,21 +77,61 @@ def get_contour(Img_PathandFilename = 'temp_image_file', opaque="--opaque", fill
                 #in this case return largest
                 item_contour = sorted_areas[0][1]
                 
+                #part of rescaling to make object touch edges of outputted SVG
+                contour_size = cv2.minAreaRect(item_contour) 
+                print >> sys.stderr, "[get_contour] Contour size",  contour_size
+                center_of_contour = [int(contour_size[0][0]),int(contour_size[0][1])]
+                print center_of_contour
+                print >> sys.stderr, "[get_contour] Contour center",  center_of_contour
+                contour_width = int(contour_size[1][0] )
+                contour_height = int(contour_size[1][1] )
+                print "contour_width;", contour_width
+                print "contour_height:", contour_height
+                scaling_factor = 1.0
+                if contour_width > contour_height: 
+                	print "contour_width is bigger"
+                	scaling_factor = scaling_factor + (1.0-(contour_width / resize_dim[0]))
+                else: 
+                	print "contour_height is bigger"
+                	scaling_factor = scaling_factor + (1.0-(contour_height / resize_dim[1]))
+                print "scaling_factor", scaling_factor
+                
                 #create a blank image
-                contour_image = np.zeros((resize_dim[1], resize_dim[0], 3), np.uint8)
-
+                #contour_image = np.zeros((resize_dim[1], resize_dim[0], 3), np.uint8)
+                #part of rescaling to make object touch edges of outputted SVG
+                contour_image = np.zeros(((resize_dim[1]*scaling_factor), scaling_factor*resize_dim[0], 3), np.uint8)
+                
+                #scaling_factor = 1.0
+                resized_contour = scaling_factor*np.array(item_contour)
+                resized_contour = resized_contour.astype(int)
+                #print item_contour[0], resized_contour[0]
+                cv2.drawContours(contour_image, [resized_contour], -1, (255, 255, 255), -1)
+                
+                leftmost = tuple(resized_contour[resized_contour[:,:,0].argmin()][0])
+                rightmost = tuple(resized_contour[resized_contour[:,:,0].argmax()][0])
+                topmost = tuple(resized_contour[resized_contour[:,:,1].argmin()][0])
+                bottommost = tuple(resized_contour[resized_contour[:,:,1].argmax()][0])
+                #print "leftmost", leftmost
+                #print "rightmost", rightmost
+                #print "topmost", topmost
+                #print "bottommost", bottommost
+                
+                #crop image to make object touch edges
+             	contour_image = contour_image[topmost[1]:bottommost[1], leftmost[0]:rightmost[0]]
                 #draw contour on blank image (fill in)
                 cv2.drawContours(contour_image, [item_contour], -1, (255, 255, 255), -1)
                 
                 #Smooth rough edges
                 #contour_image  = cv2.GaussianBlur(contour_image, (5,5), 0)
-                contour_image = cv2.dilate(contour_image,kernel,iterations = 3)
+                contour_image = cv2.dilate(contour_image,kernel,iterations = 2)
                 contour_image = cv2.erode(contour_image ,kernel,iterations = 1)
                 
                 #invert image
                 contour_image = np.invert(contour_image)
                 
                 #for now just save contour image
+                #contour_image_filename = str(uuid.uuid1())+'.bmp'
+                #cv2.imwrite(contour_image_filename, contour_image )
                 cv2.imwrite('contour_image.bmp', contour_image )
                 
                 #call potrace to convert to SVG
@@ -114,82 +154,5 @@ get_contour(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys
 #       	sys.exit(-1)  
 #
 
-'''Code below not used
-def rgb2hsv(rgb):
-    return scale_hsv( colorsys.rgb_to_hsv(norm(rgb[0]), norm(rgb[1]), norm(rgb[2])) )
 
-def norm( x):
-        return (x/255.0)
-
-def scale_hsv( hsv):
-        return ([(hsv[0]*180),(hsv[1]*255),(hsv[2]*255)])
-
-
-def remove_background(img, roi_size, bg_percent):
-                #get samples from the 4 corners of the picture
-                #this will be assumed to be background colors.
-                #taking the average of this color, remove all similar (within percentage) color from picture
-                if roi_size == None: roi_size = 25
-                if bg_percent == None: bg_percent = 0.05
-                topl = img[ 0:roi_size, 0:roi_size ]
-                topr = img[0:roi_size , (img.shape[1]-roi_size ):img.shape[1]]
-                bottom_l = img[(img.shape[0]-roi_size ):img.shape[0], 0:roi_size ]
-                bottom_r = img[(img.shape[0]-roi_size ):img.shape[0], (img.shape[1]-roi_size ):img.shape[1]]
-
-                #get avg color
-                avg_bg_color_RGB = np.mean(np.array([cv2.mean(topl), cv2.mean(topr), cv2.mean(bottom_l), cv2.mean(bottom_r)]).astype(int), axis=0)
-                #avg_bg_color = np.rint([avg_bg_color[0],  avg_bg_color[1],  avg_bg_color[2]] 
-                R_lower = int(avg_bg_color_RGB[0] - (avg_bg_color_RGB[0] * bg_percent))
-                if R_lower < 0: R_lower = 0
-                R_upper = int(avg_bg_color_RGB[0] + (avg_bg_color_RGB[0] * bg_percent))
-                if R_upper >255: R_upper = 255
-                G_lower = int(avg_bg_color_RGB[1] - (avg_bg_color_RGB[1] * bg_percent))
-                if G_lower < 0: G_lower = 0
-                G_upper = int(avg_bg_color_RGB[1] + (avg_bg_color_RGB[1] * bg_percent))
-                if G_upper >255: G_upper = 255
-                B_lower = int(avg_bg_color_RGB[2] - (avg_bg_color_RGB[2] * bg_percent))
-                if B_lower < 0: B_lower = 0
-                B_upper = int(avg_bg_color_RGB[2] + (avg_bg_color_RGB[2] * bg_percent))
-                if B_upper >255: B_upper = 255
-                
-                lower_RGB = np.array([R_lower, G_lower, B_lower]) 
-                upper_RGB = np.array([R_upper, G_upper, B_upper])
-                lower_HSV = np.array(rgb2hsv([R_lower, G_lower, B_lower])) 
-                upper_HSV = np.array(rgb2hsv([R_upper, G_upper, B_upper]))
-                #lower_HSV = np.array([20,0,0])
-                upper_HSV = np.array([180,255,255])
-                img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-                                
-                # lower mask
-                mask0 = cv2.inRange(img, lower_RGB, upper_RGB)
-                
-                print >> sys.stderr, "avg_bg_color_RGB:", avg_bg_color_RGB
-                print >> sys.stderr, 'RGB_lower:', R_lower, G_lower, B_lower 
-                print >> sys.stderr, 'RGB_upper:', R_upper, G_upper, B_upper 
-                print >> sys.stderr, 'lower_HSV:', lower_HSV
-                print >> sys.stderr, 'upper_HSV:', upper_HSV
-                print >> sys.stderr, 'mask0:', mask0.shape
-                
-                # upper mask (170-180)
-                #lower_red = np.array([170,50,50])
-                #upper_red = np.array([180,255,255])
-                #mask1 = cv2.inRange(img_hsv, lower_red, upper_red)
-
-                # set my output img to zero everywhere except my mask
-                #output_img = img.copy()
-                #output_img[np.where(mask==0)] = 0
-
-                # or your HSV image, which I *believe* is what you want
-                output_hsv = img.copy()
-                output_hsv[np.where(mask0==0)] = 0
-                #output_hsv = cv2.bitwise_and(img,img, mask= mask0)
-                output_rgb = cv2.cvtColor(output_hsv, cv2.COLOR_HSV2RGB)
-                
-                cv2.imwrite('background_mask.jpg', mask0)
-                cv2.imwrite('background_removed.jpg', output_hsv)
-                cv2.imwrite('image_HSV.jpg', img_hsv)
-                # convert to hsv and find range of colors
-                #hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-                
-'''
 
